4a2b4cf53a0d610c3305624874595e7bea44b71d
Azad2181 <abulkalamazad2181@gmail.com>
2026-01-05 10:42:31 +0600

    project file add

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Abul Kalam Azad',
+    author_email= 'abulkalamazad2181@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
